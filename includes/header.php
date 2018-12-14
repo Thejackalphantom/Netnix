81a601e02c7a044cbc9711e68c3030e4147147a6
DESKTOP-BQHDEF9\Remy Conen <remy.conen@hotmail.com>
2018-12-14 11:58:55 +0100

    Changed some things in the header.php file as of now it does not contain PHP but in the future when the searchbar and language switcher get's added this will then include php. Changed some things in Index and login.php with the foundation it has been build on. CSS is also edited allot to fix some typos and fix some issues with wrong commands.

--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,16 @@
-      <?php
-            echo "<div id='header'> 
-                    <div id='headerInside'> 
-                        <div id='logo'>          
-                            <img src='img/logo.png' id='logoResize'> 
-                        </div>
-                        <div id='menu'>          
-                            <ul>
-                                <o><a href='dummylink'class='menu_remove'>VIDEOS</a></o>
-                                <o><a href='dummylink'class='menu_remove'>ACCOUNT</a></o>
-                                <o><a href='dummylink'class='menu_remove'>UPLOAD</a></o>
-                                <o><a href='dummylink'class='menu_remove'>FAVORIET</a></o>
-                            </ul>
-                        </div>
-                    </div>
-                    <hr></hr>
-                </div>";
-            echo"
-           
-
-
-                    ";
-           
-            
-      ?>      
+<div id="header"> 
+    <div id="headerInside"> 
+        <div id="logo">          
+            <img src="img/logo.png" id="logoResize"> 
+        </div>
+        <div id="menu">          
+            <ul>
+                <li><a href="dummylink">VIDEOS</a></li>
+                <li><a href="dummylink">ACCOUNT</a></li>
+                <li><a href="dummylink">UPLOAD</a></li>
+                <li><a href="dummylink">FAVORIET</a></li>
+            </ul>
+        </div>
+    </div>
+    <hr></hr>
+</div>
--- index.php
+++ index.php
@@ -1,26 +1,20 @@
        <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
 INF1C Informatica NHL STENDEN
 -->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
         <link rel="stylesheet" type="text/css" href="css/index.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Netnix</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
         <div id="Wrap">
-            <div id="Header">
+            <?php include ("includes/header.php");?>
+            <div id="MainContent">
                 
             </div>
-            <div id="MainContent"
         </div>
     </body>
 </html>
