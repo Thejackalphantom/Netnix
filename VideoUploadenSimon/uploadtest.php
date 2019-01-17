6774b622301525bae5769b5586393ec3ec63fc7a
Simon Gharnagharyan <simon.gharnagharyan@student.nhlstenden.com>
2019-01-17 13:36:08 +0100

    Update uploadtest.php

--- VideoUploadenSimon/uploadtest.php
+++ VideoUploadenSimon/uploadtest.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
+        if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+            header("location: index.php");
+            exit;
+        }
 $Host = "127.0.0.1";
 $User = "root";
 $Password = "";
-$DBName = "netnix";  
+$DBName = "netnix";
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +17,8 @@ $DBName = "netnix";
     </head>
     <body>
         <?php
-        $Conn = mysqli_connect($Host, $User, $Password, $DBName );
+        
+        $Conn = mysqli_connect($Host, $User, $Password, $DBName);
         if ($Conn === FALSE) {
 
             echo "Failed to connected";
@@ -38,45 +43,44 @@ $DBName = "netnix";
                 $uploadOk = 1;
 
                 $mimetype = array("video/mp4");
-                if(in_array($_FILES['fileToUpload']['type'],$mimetype)){
+                if (in_array($_FILES['fileToUpload']['type'], $mimetype)) {
                     echo"This is not the correct file type please upload in mp4";
-                }else{
-                    
-                    $uploadOk = 0;
-                    if ($uploadOk == 0)
-                        { // if everything is ok, try to upload file
-                            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-                        }
+                } else {
 
-                $name = htmlentities($_FILES['fileToUpload']['name']);
-                $pathtotal = "uploads/" . $name . "";
-                        mysqli_select_db($Conn, $DBName);
-                        $SQLstring2 = "INSERT INTO videos (videoUploadPath) VALUES (?)";
-                        if ($stmt = mysqli_prepare($Conn, $SQLstring2)) {
-                            mysqli_stmt_bind_param($stmt, 's', $pathtotal);
-                            $QueryResult2 = mysqli_stmt_execute($stmt);
-                            if ($QueryResult2 === FALSE) {
-                                echo "<p>Unable to execute the query.</p>"
-                                . "<p>Error code "
-                                . mysqli_errno($Conn)
-                                . ": "
-                                . mysqli_error($Conn)
-                                . "</p>";
-                            } else {
-                                echo "yay";
-                                //header("Location: login.php");
-                            }
-                            mysqli_stmt_close($stmt);
-                        }
-                        mysqli_close($Conn);
+                    $uploadOk = 0;
+                    if ($uploadOk == 0) { // if everything is ok, try to upload file
+                        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
                     }
                 }
-            } 
+
+                    $name = htmlentities($_FILES['fileToUpload']['name']);
+                    $pathtotal = "uploads/" . $name . "";
+                    mysqli_select_db($Conn, $DBName);
+                    $SQLstring2 = "INSERT INTO videos (videoUploadPath) VALUES (?)";
+                    if ($stmt = mysqli_prepare($Conn, $SQLstring2)) {
+                        mysqli_stmt_bind_param($stmt, 's', $pathtotal);
+                        $QueryResult2 = mysqli_stmt_execute($stmt);
+                        if ($QueryResult2 === FALSE) {
+                            echo "<p>Unable to execute the query.</p>"
+                            . "<p>Error code "
+                            . mysqli_errno($Conn)
+                            . ": "
+                            . mysqli_error($Conn)
+                            . "</p>";
+                        } else {
+                            echo "Upload succesful!";
+                            //header("Location: login.php");
+                        }
+                        mysqli_stmt_close($stmt);
+                    }
+                    mysqli_close($Conn);
+                }
+            }
         ?>
         <div id="wrap">
             <div id="mainContainer">
                 <div id="header">
-                    
+
                 </div>
                 <div id="menu">
                     <div class="button"><a href="index.php">Home</a></div>
@@ -85,8 +89,8 @@ $DBName = "netnix";
                     <div id="register">
                         <form id="formSignUp" action="uploadtest.php" method="POST" enctype="multipart/form-data" >
                             <p>Please upload your video</p>
-                            <p><input type="file" name="fileToUpload" id="fileToUpload"></p>
-                            <p><input type="submit" name="submit" value="add Video"><input type="reset" name="reset" value="Reset form"></p>
+                            <p><input type="file" name="fileToUpload" id="fileToUpload" required></p>
+                            <p><input type="submit" name="submit" value="add Video"></p>
                         </form>
                     </div>
                 </div>
