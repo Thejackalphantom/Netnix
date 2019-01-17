2e967630fc3d9c925f43361145c4bb97d751fe52
MichielArnoldus <43609579+MichielArnoldus@users.noreply.github.com>
2019-01-17 10:08:36 +0100

    small fixes

--- includes/CategorieBlok.php
+++ includes/CategorieBlok.php
@@ -1,18 +1,18 @@
 <div class="imageRow">
     <div class="pLinks">
-        <a href="includes/Informatica.php">
+        <a href="Informatica.php">
             <img src="img/informatica_logo.png" id="Inf-Logo">
             <p>informatica</p>
         </a>
     </div>
     <div class="pLinks">
-        <a href="includes/PABO.php">
+        <a href="PABO.php">
             <img src="img/pabo_logo.png" id="Java-Logo">
             <p>PABO</p>
         </a>
     </div>
     <div class="pLinks">
-        <a href="includes/HotelSchool.php">
+        <a href="HotelSchool.php">
             <img src="img/hotelschool_logo.png" id="PHP-logo">
             <p>HotelSchool</p>
         </a>
