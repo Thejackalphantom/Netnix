86eab04ff632367275749215912d9e89641ab440
Simon Gharnagharyan <simon.gharnagharyan@student.nhlstenden.com>
2019-01-18 14:06:38 +0100

    Index
    Index laat nu videos zien

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php // Begin maken aan de sessie
-        session_start();
+<?php
+// Begin maken aan de sessie
+session_start();
 
-        if(!isset($_SESSION['loggedin']))
-        {
-            // not logged in
-            header('Location: login.php');
-            exit();
-        }
+if (!isset($_SESSION['loggedin'])) {
+    // not logged in
+    header('Location: login.php');
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -21,32 +21,45 @@ INF1C Informatica NHL STENDEN
         <title>Netnix</title>
     </head>
     <body>
+        <?php
+        $mysqli = new mysqli('127.0.0.1', 'root', '', 'netnix') or die($mysqli->connect_error);
+        $table = 'videos';
+
+        $result = $mysqli->query("SELECT videoID, videoDescription, videoUploadPath FROM $table") or die($mysqli->error);
+        ?>
         <div id="Wrap">
             <div id="content">
-                <?php include ("includes/header.php");?>
+                <?php include ("includes/header.php"); ?>
                 <div id="MainContent">
                     <div class="video">
-                        <h2 class="title"> AANBEVOLEN </h2>   
+                        <h2 class="title"> AANBEVOLEN </h2>
                         <hr>
                         <div class="videoBox">
-                            
+
                         </div>
                         <div class="videoBox">
-                            
+
                         </div>
                         <div class="videoBox">
-                            
+
                         </div>
                         <div class="videoBox">
-                            
+
                         </div>
                     </div>
                     <div class="video">
                         <h2 class="title"> VIDEOS </h2>   
-                        <hr>
-                    </div>   
+                        <div class="videoBox">
+                            <?php
+                            while ($data = $result->fetch_assoc()) {
+                                //print_r($data);
+                                echo "<h2>{$data['videoDescription']}</h2>";
+                                echo "<iframe width='395 height='400' src='{$data['videoUploadPath']}'></iframe>";
+                            }
+                            ?>
+                        </div>   
+                    </div>
                 </div>
             </div>
-        </div>
     </body>
 </html>
