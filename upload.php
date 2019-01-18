e2c04823ad394ed08351c1d2f5eb72a0836b6b0f
Simon Gharnagharyan <simon.gharnagharyan@student.nhlstenden.com>
2019-01-18 12:02:49 +0100

    Video Upload Klaar en Werkend

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] != true) {
-    header("location: login.php");
+    header("location: index.php");
     exit;
 }
 ?>
@@ -50,72 +50,71 @@ Upload page
                         </form>  
 
                         <?php
-        $Conn = mysqli_connect("localhost", "root", "");
-        if ($Conn === FALSE) {
+                        $Conn = mysqli_connect("localhost", "root", "");
+                        if ($Conn === FALSE) {
 
-            echo "Failed to connected";
-        } else {
-            $DBName = "netnix";
-            if (!mysqli_select_db($Conn, $DBName)) {
+                            echo "Failed to connected";
+                        } else {
+                            $DBName = "netnix";
+                            if (!mysqli_select_db($Conn, $DBName)) {
 
-                $SQLstring = "CREATE DATABASE netnix";
-                if ($stmt = mysqli_prepare($Conn, $SQLstring)) {
-                    $QueryResult = mysqli_stmt_execute($stmt);
-                    if ($QueryResult === FALSE) {
-                        echo "<p>Well thats a error!</p>";
-                    } else {
-                        echo "<p>You are the first visitor!</p>";
-                    }
-                    mysqli_stmt_close($stmt);
-                }
-            }
-
-            if (isset($_POST['submit'])) {
-                $target_dir = "uploads/";
-                $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-                $uploadOk = 1;
-
-                $mimetype = array("image/mp4","image/wav");
-                if(in_array($_FILES['fileToUpload']['type'],$mimetype)){
-                    echo"This is not the correct file type please upload in .WAV/.MP4";
-                }else{
-                    
-                    $uploadOk = 0;
-                    if ($uploadOk == 0)
-                        { // if everything is ok, try to upload file
-                            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-                        }
-
-                $name = htmlentities($_FILES['fileToUpload']['name']);
-                $categorie = htmlentities($_POST['categorie']);
-                $title = htmlentities($_POST['title']);
-                $message = htmlentities($_POST['message']);
-                $pathtotal = "uploads/" . $name . "";
-                $userid = $_SESSION['id'];
-                
-                        mysqli_select_db($Conn, $DBName);
-                        $SQLstring2 = "INSERT INTO videos VALUES(NULL, ?, ?, ?, ?, ?)";
-                        if ($stmt = mysqli_prepare($Conn, $SQLstring2)) {
-                            mysqli_stmt_bind_param($stmt, 'sssss', $userid, $title, $message,$pathtotal, $categorie);
-                            $QueryResult2 = mysqli_stmt_execute($stmt);
-                            if ($QueryResult2 === FALSE) {
-                                echo "<p>Unable to execute the query.</p>"
-                                . "<p>Error code "
-                                . mysqli_errno($Conn)
-                                . ": "
-                                . mysqli_error($Conn)
-                                . "</p>";
-                            } else {
-                                echo "Bedankt voor het uploaden van een video.";
-                                //header("Location: login.php");
+                                $SQLstring = "CREATE DATABASE netnix";
+                                if ($stmt = mysqli_prepare($Conn, $SQLstring)) {
+                                    $QueryResult = mysqli_stmt_execute($stmt);
+                                    if ($QueryResult === FALSE) {
+                                        echo "<p>Well thats a error!</p>";
+                                    } else {
+                                        echo "<p>You are the first visitor!</p>";
+                                    }
+                                    mysqli_stmt_close($stmt);
+                                }
                             }
-                            mysqli_stmt_close($stmt);
+
+                            if (isset($_POST['submit'])) {
+                                $target_dir = "uploads/";
+                                $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+                                $uploadOk = 1;
+
+                                $mimetype = array("image/mp4", "video/mov", "video/wmv", "video/flv");
+                                if (in_array($_FILES['fileToUpload']['type'], $mimetype)) {
+                                    echo"<br><h3>This is not the correct file type, please upload a video!</h3>";
+                                } else {
+
+                                    $uploadOk = 0;
+                                    if ($uploadOk == 0) { // if everything is ok, try to upload file
+                                        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+                                    }
+
+                                    $name = htmlentities($_FILES['fileToUpload']['name']);
+                                    $categorie = htmlentities($_POST['categorie']);
+                                    $title = htmlentities($_POST['title']);
+                                    $message = htmlentities($_POST['message']);
+                                    $pathtotal = "uploads/" . $name . "";
+                                    $userid = $_SESSION['id'];
+
+                                    mysqli_select_db($Conn, $DBName);
+                                    $SQLstring2 = "INSERT INTO videos VALUES(NULL, ?, ?, ?, ?, ?)";
+                                    if ($stmt = mysqli_prepare($Conn, $SQLstring2)) {
+                                        mysqli_stmt_bind_param($stmt, 'sssss', $userid, $title, $message, $pathtotal, $categorie);
+                                        $QueryResult2 = mysqli_stmt_execute($stmt);
+                                        if ($QueryResult2 === FALSE) {
+                                            echo "<p>Unable to execute the query.</p>"
+                                            . "<p>Error code "
+                                            . mysqli_errno($Conn)
+                                            . ": "
+                                            . mysqli_error($Conn)
+                                            . "</p>";
+                                        } else {
+                                            echo "<br><h3>Bedankt voor het uploaden van een video.</h3>";
+                                            //header("Location: login.php");
+                                        }
+                                        mysqli_stmt_close($stmt);
+                                    }
+                                    mysqli_close($Conn);
+                                }
+                            }
                         }
-                        mysqli_close($Conn);
-                    }
-                }
-            } 
-        ?>
+                        ?>
                     </div>
                 </div>
             </div>
