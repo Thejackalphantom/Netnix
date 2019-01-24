f4d1c0f2ca8bacf9ff6442a8d34f42263e199235
Bart <bart.wijshake@student.nhlstenden.com>
2019-01-24 10:38:16 +0100

    Begonnen met taal functie synchroniseren, elke pagina die in 'DYNAMIC' staat is goed.

---
+++ test/Netnix_Test/css/includes/CategorieBlok.php
@@ -0,0 +1,20 @@
+<div class="imageRow">
+    <div class="pLinks">
+        <a href="includes/Informatica.php">
+            <img src="img/informatica_logo.png" alt="Inf-Logo">
+            <p>informatica</p>
+        </a>
+    </div>
+    <div class="pLinks">
+        <a href="includes/PABO.php">
+            <img src="img/pabo_logo.png" alt="Java-Logo">
+            <p>PABO</p>
+        </a>
+    </div>
+    <div class="pLinks">
+        <a href="includes/HotelSchool.php">
+            <img src="img/hotelschool_logo.png" alt="PHP-logo">
+            <p>Hotel School</p>
+        </a>
+    </div>
+</div>
