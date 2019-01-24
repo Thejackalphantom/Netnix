4e1bc815326e8cf7d65f97d523e9e3cd124242b6
DESKTOP-BQHDEF9\Remy Conen <remy.conen@hotmail.com>
2019-01-24 10:39:23 +0100

    edited the config files

--- config.php
+++ config.php
@@ -1,17 +1,14 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$server = "127.0.0.1";
-$username = "admin";
-$password = "Admin";
-$DBname = "netnix";
+/* Database credentials. Assuming you are running MySQL*/
+$Server = "127.0.0.1"; //database hosted connection
+$Admin = "admin"; //name of the Database acount
+$Pass = "Admin"; // database password
+$DBname = "netnix"; // database name
  
     /* Attempt to connect to MySQL database */
-    $link = mysqli_connect($server, $username, $password, $DBname);
+    $Conn = mysqli_connect($Server, $Admin, $Pass, $DBname);
 
     // Check connection
-    if($link === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
+    if($Conn === false){
+        die("ERROR: Could not connect tothe database please try again later. " . mysqli_connect_error());
     }
-    
-    //echo "connection established";
