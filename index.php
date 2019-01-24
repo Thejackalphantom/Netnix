d2968f30cfeb71128ef34973322ecc781d9a1cbb
DESKTOP-BQHDEF9\Remy Conen <remy.conen@hotmail.com>
2019-01-24 15:36:21 +0100

    Added "aanbevolen" support for now it will pick random video's on refresh

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ if (!isset($_SESSION['loggedin'])) {
     exit();
 }
 ?>
-
 <!DOCTYPE html>
 <!--
 INF1C Informatica NHL STENDEN
@@ -26,26 +25,29 @@ INF1C Informatica NHL STENDEN
         $table = 'videos';
 
         $result = $mysqli->query("SELECT videoID, videoTitle, videoDescription, videoUploadPath FROM $table WHERE aprove = 1") or die($mysqli->error);
+        $random = $mysqli->query("SELECT videoID, videoTitle, videoDescription, videoUploadPath FROM $table WHERE aprove = 1 ORDER BY RAND() LIMIT 5") or die($mysqli->error);
         ?>
         <div id="Wrap">
             <div id="content">
                 <?php include ("includes/header.php"); ?>
                 <div id="MainContent">
                     <div class="video">
-                        <h2 class="title"> AANBEVOLEN </h2>
-                        <hr>
-                        <div class="videoBox">
-
-                        </div>
-                        <div class="videoBox">
-
-                        </div>
-                        <div class="videoBox">
-
-                        </div>
+                        <h2 class="title"> Willekeurige video's </h2>
+                        <?php
+                            while ($data2 = $random->fetch_assoc()) {
+                                //print_r($data);
+                                echo "<a href=videoshow.php?videoid={$data2['videoID']}><div class='videoBoxUser'>
+                                    <h2>{$data2['videoTitle']}</h2>
+                                        <video width='300' height='300'>
+                                        <source src='{$data2['videoUploadPath']}' type=video/mp4>
+                                        <source src='{$data2['videoUploadPath']}' type=video/wav>
+                                        </video>
+                                </div></a>";
+                            }
+                            ?> 
                     </div>
                     <div class="video">
-                        <h2 class="title"> VIDEOS </h2>   
+                        <h2 class="title"> Geüploade video's </h2>   
                             <?php
                             while ($data = $result->fetch_assoc()) {
                                 //print_r($data);
