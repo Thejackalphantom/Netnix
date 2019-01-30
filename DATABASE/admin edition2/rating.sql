cd5b820272f79ed60441fa9990477ca8d150b19e
THIJSLAPTOP\Gebruiker <thijsrijkers@live.nl>
2019-01-30 12:08:03 +0100

    rating database

---
+++ DATABASE/admin edition2/rating.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 30 jan 2019 om 12:07
+-- Serverversie: 10.1.37-MariaDB
+-- PHP-versie: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `netnix`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `rating`
+--
+
+CREATE TABLE `rating` (
+  `videoID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `rating` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `rating`
+--
+
+INSERT INTO `rating` (`videoID`, `userID`, `rating`) VALUES
+(5, 1, 5),
+(5, 2, 5),
+(5, 3, 5),
+(5, 6, 5);
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`userID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
